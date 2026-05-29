219a27f254ed392a560d74e928be1700a3426421
𝒀𝑨𝑵𝑰𝑺 <yanisredjradjyanis@gmail.com>
2026-05-29 17:01:21 +0100

    fix hamburger menu & fix sound bottom

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -51,12 +51,25 @@ function Header() {
       </button>
 
       <div className={`mobile-menu ${menuOpen ? "open" : ""}`}>
-        <a href="#hero" onClick={() => setMenuOpen(false)}>Home</a>
-        <a href="#about" onClick={() => setMenuOpen(false)}>About</a>
-        <a href="#project" onClick={() => setMenuOpen(false)}>Project</a>
-        <a href="#serv" onClick={() => setMenuOpen(false)}>Services</a>
-        <a href="#contact" onClick={() => setMenuOpen(false)}>Contact</a>
-      </div>
+  <a href="#hero" onClick={() => setMenuOpen(false)}>Home</a>
+  <a href="#about" onClick={() => setMenuOpen(false)}>About</a>
+  <a href="#project" onClick={() => setMenuOpen(false)}>Project</a>
+  <a href="#serv" onClick={() => setMenuOpen(false)}>Services</a>
+  <a href="#contact" onClick={() => setMenuOpen(false)}>Contact</a>
+
+  <div className="mobile-social">
+    <a href="https://www.instagram.com/yanis_redjradj" target="_blank" rel="noreferrer"><FaInstagram /></a>
+    <a href="https://discord.com/users/yanis_rj" target="_blank" rel="noreferrer"><FaDiscord /></a>
+    <a href="https://www.tiktok.com/@yanis.redjradj" target="_blank" rel="noreferrer"><FaTiktok /></a>
+    <a href="https://www.snapchat.com/add/yanis.redjradj" target="_blank" rel="noreferrer"><FaSnapchatGhost /></a>
+    <a href="https://www.facebook.com/share/18XRkSPbaR/?mibextid=wwXIfr" target="_blank" rel="noreferrer"><FaFacebook /></a>
+    <a href="https://x.com/yanis_redjradj" target="_blank" rel="noreferrer"><FaXTwitter /></a>
+    <a href="https://wa.me/213676938038" target="_blank" rel="noreferrer"><FaWhatsapp /></a>
+    <a href="https://t.me/+213676938038" target="_blank" rel="noreferrer"><FaTelegram /></a>
+    <a href="https://github.com/yanisredjradj" target="_blank" rel="noreferrer"><FaGithub /></a>
+    <a href="https://open.spotify.com/user/31vn3tquljxh3uavv3zqmqzkx3qm?si=35295d2b7b264633" target="_blank" rel="noreferrer"><FaSpotify /></a>
+  </div>
+</div>
     </header>
   )
 }
